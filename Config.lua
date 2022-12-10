d07b70d03c17674a033dabcdd1fce294a493cf81
xNsZen <92114952+ExZeroz@users.noreply.github.com>
2022-12-10 23:55:18 +0700

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,6 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
+	SaveManager.Folder = 'Xeris Hub'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
@@ -186,7 +187,7 @@ local SaveManager = {} do
 		section:AddDropdown('SaveManager_ConfigList', { Text = 'Config list', Values = self:RefreshConfigList(), AllowNull = true })
 		section:AddInput('SaveManager_ConfigName',    { Text = 'Config name' })
 
-
+		section:AddDivider()
 
 		section:AddButton('Create config', function()
 			local name = Options.SaveManager_ConfigName.Value
@@ -240,7 +241,7 @@ local SaveManager = {} do
 			Options.SaveManager_ConfigList:SetValue(nil)
 		end)
 
-		SaveManager.AutoloadLabel = section:AddLabel('Current autoload config: none')
+		SaveManager.AutoloadLabel = section:AddLabel('Current autoload config: none', true)
 
 		if isfile(self.Folder .. '/settings/autoload.txt') then
 			local name = readfile(self.Folder .. '/settings/autoload.txt')
